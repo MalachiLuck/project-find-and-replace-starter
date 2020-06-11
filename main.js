230732eede33641ac9a5b68a8dd6a043c967bec0
Malachi Luck <malachiluck@gmail.com>
2020-06-11 17:35:48 -0500

    done

--- main.js
+++ main.js
@@ -19,20 +19,17 @@ function getCellElements (currentRowElement) {
     return currentRowElement.querySelectorAll(".cell")
 }
 
+replaceAllButton.addEventListener("click", function (){
+    let currentTxt = findInput.value
+    let replacementTxt = replaceInput.value
+    for (let rowIndex = 0; rowIndex < rowElements.length; rowIndex += 1 ) {
+        let cellArray = getCellElements(rowElements[rowIndex])
+    for (let cellIndex = 0; cellIndex < cellArray.length; cellIndex += 1) {
+        if (cellArray[cellIndex].innerText.includes(currentTxt)) {
+            cellArray[cellIndex].innerHTML = cellArray[cellIndex].innerHTML.replace(currentTxt, replacementTxt)
+        }
+    }
+    }
+}
+)
 
-// YOUR CODE GOES HERE
-
-
-// One last thing: dedicate very careful attention to using variables and
-// naming them accurately.
-// And when you change the value you are assigning to a variable, don't
-// forget to consider changing the name to reflect the change you made! It
-// is very easy to get confused when you are working inside NESTED LOOPS.
-// The best of us do. And unnecessary confusion during the process of 
-// developing your code means wasted time.
-//
-// The time-cost of structuring and naming things well is FAR less than the
-// time-cost of ignoring the quality and readability of your code.
-//
-// You can, of course, remove any comments in this starter project once
-// you have read them, if you prefer.
